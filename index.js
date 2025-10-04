a0d99873e4ec3bd8170e6faaebc617ad72ecccc0
cheeseonamonkey <johndoe@example.com>
2025-10-04 17:41:36 -0600

    a

---
+++ index.js
@@ -0,0 +1,9 @@
+(async () => {
+    const Game = await import("./classes/game/models/Game.js");
+    //   const Simulator = await import("./simulator.js");
+ 
+    let game = new Game.default();
+    game.render()
+ 
+ 
+ })();
